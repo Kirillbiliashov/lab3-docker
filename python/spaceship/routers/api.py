d19edfc9f06e36aa08d80f530e9ebb524cfac930
Kirillbiliashov <Kirillbeliashov14@gmail.com>
2023-03-30 20:23:04 +0300

    route function was moved to another file

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,8 +1,19 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
 
-
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def multiply_matrices() -> dict:
+    matrix1 = np.random.rand(10, 10)
+    matrix2 = np.random.rand(10, 10)
+    product = np.dot(matrix1, matrix2)
+    return {
+        "matrix_a": matrix1.tolist(),
+        "matrix_b": matrix2.tolist(),
+        "product": product.tolist()
+    }
